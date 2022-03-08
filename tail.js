b8f061dcda683e69054e9b90d55d357c8825eb23
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-08 03:39:27 +0000

    tail.js compares the tail of an array and another value

---
+++ tail.js
@@ -0,0 +1,14 @@
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔥🔥🔥Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
