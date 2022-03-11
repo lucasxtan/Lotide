5a26c685b7ebf7f7bae6ba498d28d5f32f177093
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-11 04:17:45 +0000

    add findKeys.js

--- takeUntil.js
+++ takeUntil.js
@@ -33,6 +33,31 @@ const takeUntil = function (array, callback) {
   return takeArray;
 }
 
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length){
+    return false
+  }
+  for (let i = 0; i < a.length; i++){
+    if (a[i] !== b[i]){
+      return false
+    } 
+  }
+  return true
+}
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a, b)) {
+    console.log(`✅✅✅Assertion Passed: [${a}] === [${b}]`);
+  } else {
+    console.log(`🔥🔥🔥Assertion Failed: [${a}] !== [${b}]`);
+  }
+};
+
+
+
+
+
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -42,6 +67,10 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
 
 //should output
 // [ 1, 2, 5, 7, 2 ]
